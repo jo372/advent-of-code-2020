a7a0ef7acb33384462124e95e9e77d08c8032c1f
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2021-01-29 02:14:38 +0000

    updated to have partOne and partTwo seperated, not very dynamic but it will do for now...

--- day1/src/com/company/ReportRepair.java
+++ day1/src/com/company/ReportRepair.java
@@ -8,18 +8,34 @@ import java.util.Scanner;
 
 public class ReportRepair {
     private static List<Integer> inputData = new ArrayList<>();
-    public static void findResult() {
+    public static void partOne() {
         int value1, value2;
         for(int i=0; i < inputData.size(); i++) {
             value1 = inputData.get(i);
             for(int i2=i+1; i2 < inputData.size(); i2++) {
                 value2 = inputData.get(i2);
                 if(2020 - value1 - value2 == 0) {
-                    System.out.printf("%d * %d = %d\n", value1, value2, value1 * value2);
+                    System.out.printf("Part One: %d * %d = %d\n", value1, value2, value1 * value2);
                 }
             }
         }
     }
+    public static void partTwo() {
+        int value1, value2, value3;
+        for(int i=0; i < inputData.size(); i++) {
+            value1 = inputData.get(i);
+            for(int i2=i+1; i2 < inputData.size(); i2++) {
+                value2 = inputData.get(i2);
+                for(int i3=i2+1; i3 < inputData.size(); i3++) {
+                    value3 = inputData.get(i3);
+                    if(2020 - value1 - value2 - value3 == 0) {
+                        System.out.printf("Part Two: %d * %d * %d = %d\n", value1, value2, value3, value1 * value2 * value3);
+                    }
+                }
+            }
+        }
+    }
+    
     public static void loadData() {
         try {
             File file = new File("data/input.txt");
@@ -37,7 +53,8 @@ public class ReportRepair {
 	    loadData();
 	    // data has loaded when the inputData size is greater than zero.
 	    if(inputData.size() > 0) {
-            findResult();
+            partOne();
+            partTwo();
         }
     }
 }
